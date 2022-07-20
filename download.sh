a1bca855cbfdd377a99320a1bdd602b1a6f144f1
Superb Owl <owl@superb-owl.link>
2022-07-20 09:32:03 -0400

    add descendent counts

--- download.sh
+++ download.sh
@@ -13,16 +13,37 @@ echo "Getting data for $username"
 
 IDS=$(curl -fsL "$API_BASE/user/$username.json" | jq '.submitted[]')
 
+function count_kids_recursive() {
+  sleep 1 # avoid rate limiting
+  id=$1
+  data=$(curl -fsL "$API_BASE/item/$id.json")
+  if [[ $(echo $data | jq ".kids") == "null" ]]; then
+    echo "0"
+    return
+  fi
+
+  total=0
+  kids=$(echo $data | jq -r ".kids[]")
+  for kid in $kids; do
+    kid_total=$(count_kids_recursive $kid)
+    >&2 echo "kid total $kid_total"
+    total=$((total + kid_total + 1))
+  done
+  >&2 echo "total $total"
+  echo $total
+}
+
 echo "window.data = [" > data.js
 for ID in $IDS
 do
-  echo $ID
   html=$(curl -fsL --cookie "$HN_COOKIE" -L "$HTML_BASE/item?id=$ID")
   score=$(echo $html | pup 'table.fatitem .score json{}' | jq -r ".[0].text" | sed 's/ points\?//')
-  echo $score
 
   data=$(curl -fsL "$API_BASE/item/$ID.json")
+  descendants=$(count_kids_recursive $ID)
+  echo "$ID $score $descendants"
   data=$(echo "${data}" | jq ". +={\"score\": $score}")
+  data=$(echo "${data}" | jq ". +={\"descendants\": $descendants}")
   echo "${data}" >> data.js
   echo "," >> data.js
 
